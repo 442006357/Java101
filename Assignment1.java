e6849a5ede20ec38a5fa062f675495444faa9d7e
442006357 <91495712+442006357@users.noreply.github.com>
2021-09-27 22:26:08 +0300

    Create Assignment1.java

---
+++ Assignment1.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+class Main {
+ public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);
+    float num1;
+    float num2;
+    System.out.println("Enter a num");
+    num1 = myObj.nextFloat();
+    System.out.println("Enter a num");
+    num2 = myObj.nextFloat();
+    System.out.println(num1+num2);
+    System.out.println(num1-num2);
+    System.out.println(num1*num2);
+    System.out.println(num1/num2);
+    System.out.println(num1%num2);
+    System.out.println(num1>num2);
+    System.out.println(num1>=num2);
+    System.out.println(num1<num2);
+    System.out.println(num1<=num2);
+    System.out.println(num1!=num2);
+    System.out.println(num1==num2);
+   }
+ }
+//YAZN ALKIRI
+//442006357
