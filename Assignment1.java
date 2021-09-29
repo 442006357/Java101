d37fe1d50a1f0acb1362560e8fb4cf05e69e41d6
Hani M. Th <hmthubaiti@uqu.edu.sa>
2021-09-30 00:28:48 +0300

    fix the only issue

--- Assignment1.java
+++ Assignment1.java
@@ -1,5 +1,7 @@
-
+//@ move line 2 to be in line 1
 import java.util.Scanner;
+//your grade is 4.5/5
+//@ change the file name Main to Assignment1 ( Golden rule )
 class Main {
  public static void main(String[] args) {
     Scanner myObj = new Scanner(System.in);
