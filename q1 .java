bc5e24b2582ef355d26bd321d04d1c506adbb8de
442006357 <91495712+442006357@users.noreply.github.com>
2021-10-12 09:17:43 +0300

    Create q1 .java

---
+++ q1 .java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Main {
+ public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);
+    
+    x = nextFloat()
+   System System.out.println(2%);
+else
+int System System.out.println();
+
+if
+
+else
+int System System.out.println();
+
+Scanner
