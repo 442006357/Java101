dab142e84b355f4e9e72f8af43e2b2ddf11a8564
442006357 <91495712+442006357@users.noreply.github.com>
2021-10-20 20:26:49 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner
+public class Main.java
+{
+	public static void main(String[] args) {
+Scanner scan = new Scanner (System.in);
+
+
+
+{
+else if (the resuit is less than 50 than print the following);
+System.out.println(is less than 50%2);
+}
+
+{
+else (println the following)
+System.out.println(is greater or equal than 50%2);
+}
+if (100)
+System.out.println("error");
+
+
+}
