1419cb03d801692c1c33ab9a417ba44a44879a6c
442006357 <91495712+442006357@users.noreply.github.com>
2021-12-14 23:28:02 +0300

    Create Stars.java

---
+++ Stars.java
@@ -0,0 +1,18 @@
+public class Stars.java  {
+public static void main(String[] args) {
+        int p = 0;
+        while(p <= 5){
+        int g = 1;
+        while(g <= p){
+        System.out.print("*");
+        ++g;
+        }
+
+        System.out.println("*");
+
+        ++p;
+        }
+        }
+        }
+        
+//YAZN
