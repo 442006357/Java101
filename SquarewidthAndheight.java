589423127f4dec77b3ffe783ecf308cb30c5c794
442006357 <91495712+442006357@users.noreply.github.com>
2021-12-14 23:27:09 +0300

    Create  SquarewidthAndheight.java

---
+++ SquarewidthAndheight.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SquarewidthAndheight.java
+        {
+public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+    
+    
+   System.out.print("Enter the height and width");
+float R;
+R = scan.nextFloat();
+float A;
+A = scan.nextFloat();
+System.out.print(R*A);
+}
+    
+        }
+//YAZN
